2ec57083feca73782f26cd187b0ce7490a0f632a
Bruno Conte <brunoedconte@gmail.com>
2018-12-10 22:46:47 +0000

    Criada função de Post Consulta e alterado arquivos de routes e Application

--- src/Handler/Consulta.hs
+++ src/Handler/Consulta.hs
@@ -9,8 +9,6 @@ module Handler.Consulta where
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
-import Text.Lucius
-import Text.Julius
 import Data.Time
 import Data.Aeson
 import Data.Aeson.Casing
@@ -34,3 +32,32 @@ instance ToJSON ConsReqJSON where
    toJSON = genericToJSON $ aesonPrefix snakeCase
 instance FromJSON ConsReqJSON where
    parseJSON = genericParseJSON $ aesonPrefix snakeCase
+   
+   
+--Função que receberá o POST de consulta
+postConsultaR :: Handler TypedContent
+postConsultaR = do
+    addHeader "ACCESS-CONTROL-ALLOW-ORIGIN" "*"
+    consjson <- requireJsonBody :: Handler ConsReqJSON
+    _ <- runDB $ get404 $ consreqPacienteid consjson
+    _ <- runDB $ get404 $ consreqMedicoid consjson
+    _ <- runDB $ get404 $ consreqEspecid consjson
+    agora <- liftIO $ getCurrentTime
+    consulta <- return $ createConsulta agora consjson
+    consultaid <- runDB $ insert consulta
+    sendStatusJSON created201 (object ["id" .= consultaid])
+    
+
+--Função que pega o tempo de agora e o JSON postado para criar o tipo Consulta (usado no banco)    
+createConsulta :: UTCTime -> ConsReqJSON -> Consulta
+createConsulta agora consjson = 
+    Consulta {
+        consultaPacienteid      = consreqPacienteid consjson,
+        consultaMedicoid        = consreqMedicoid consjson,
+        consultaEspecid         = consreqEspecid consjson,
+        consultaInicio          = zonedTimeToUTC $ consreqInicio consjson,
+        consultaTermino         = zonedTimeToUTC $ consreqTermino consjson,
+        consultaObservacoes     = consreqObservacoes consjson,
+        consultaInsertedTimestamp       = agora,
+        consultaLastUpdatedTimestamp    = agora
+    }
